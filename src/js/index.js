729b519e21cac64c1120425d0344f22a23a55805
Claudia Jimenez <claudia_jimenez_@outlook.com>
2022-08-25 22:13:58 -0500

    add dom functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,36 @@
 import { addPokemon } from './history';
 
-addPokemon('poke1');
-addPokemon('poke2');
-addPokemon('poke3');
+
+
+
+//Functionality
+
+//DOM constants
+const searchButton=document.querySelector('#searchButton')
+const pokemonImg=document.querySelector('#pokemonImg')
+const pokemonDes=document.querySelector('#pokemonDes')
+searchButton.addEventListener('click', function(){
+    const searchContent=document.querySelector('#search').textContent;
+    fetchResults(searchContent)
+})
+
+function fetchResults(secondParameter){
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${secondParameter}`)
+    .then (function(data){
+        addPokemon(data.json())
+        return data.json()
+
+    })
+
+}
+
+fetchResults
+.then(function(data){
+    renderHTML(data)
+})
+
+function renderHTML(data){
+ //render image
+ //pokemonImg.src=data.
+ pokemonDes.textContent=data
+}
